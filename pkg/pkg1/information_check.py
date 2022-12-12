baf325b88a47af5d78b6d48b84ea4ae18514c1b6
lilitang2022 <112470886+lilitang2022@users.noreply.github.com>
2022-12-12 15:07:01 -0800

    Add files via upload

--- pkg/pkg1/information_check.py
+++ pkg/pkg1/information_check.py
@@ -1,11 +1,25 @@
 class Info_Check():
     
-    def __init__(self,input):
+    def __init__(self,name,iid):
         self.list = ["12345","54321"]
-        self.input = input
+        self.name = name
+        self.iid = iid
     
     def check_inlist(self):
         if self.input.lower() in self.list:
             return True
         else:
             return False
+        
+    def check_name(self):
+        if self.name==null:
+            return True
+        else:
+            return False
+        
+    def check_ID(self):
+        if self.iid==null:
+            return True
+        else:
+            return False
+    
