661a96367aa6022c39b8671e2a1bb9bc6280d3f4
Pak Lam (Justin) Chan <61757423+Just249@users.noreply.github.com>
2022-12-22 18:25:27 -0800

    Add files via upload

---
+++ ubcmds/pkg/pkg1/information_input.py
@@ -0,0 +1,27 @@
+def group():
+        g=input("Are you a student or a teacher?")
+        return g
+    
+def name():
+        n=input("What's your name?")
+        return n
+    
+def ID():   
+        I=input("What's your ID? ")
+        return I
+
+# a student
+def major():   
+        n=input("What's your major? ")
+        return n
+def degree():
+        n=input("Are you an undergraduate or postgraduate?")
+        return n
+# a teacher    
+def research():
+        n=input("what is your research direction?")
+        return n
+    
+def length():
+        n=input("How many years since be a tearcher?")
+        return n
