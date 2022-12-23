65eb4799b20754c79d6bc03cc6eb8bdb86fa0a06
Pak Lam (Justin) Chan <61757423+Just249@users.noreply.github.com>
2022-12-22 18:22:36 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='mdsquizapp',
-    version='0.2',
+    version='0.1',
     packages=find_packages(exclude=['test*']),
     license='MIT',
     description='MDS quiz package',
-	url='https://github.com/Just249/DATA533_OOPProject/tree/main/mdsquizapp',
+	url='https://github.com/Just249/DATA533_OOPProject',
     author='Justin',
     author_email='chanpaklam321@gmail.com'
 )
